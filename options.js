361f9deff473dadf7520d47a250a24b681895276
Adriaan van Rossum <git@adriaan.io>
2020-06-03 12:17:38 +0200

    Make innerHTMl safe

--- options.js
+++ options.js
@@ -1,64 +1,84 @@
-let blacklist = []
+let blacklist = [];
 
-chrome.storage.local.get(['blacklist'], ({ blacklist: blacklistLocal }) => {
-  if (blacklistLocal) blacklist = blacklistLocal
+chrome.storage.local.get(["blacklist"], ({ blacklist: blacklistLocal }) => {
+  if (blacklistLocal) blacklist = blacklistLocal;
 
-  const websites = []
+  const websitesSafe = [];
   blacklist.forEach(({ hostname, enabled, timesBlocked }) => {
-    websites.push(`
+    const hostnameSafe = hostname.replace(/[^a-z0-9./?-]+/gi, "");
+    websitesSafe.push(`
       <li>
-        <label class="checkbox" for="${hostname}">
-          <input type="checkbox" id="${hostname}" ${enabled ? 'checked' : ''}>
-          <span class="checkmark"></span> ${hostname}
+        <label class="checkbox" for="${hostnameSafe}">
+          <input type="checkbox" id="${hostnameSafe}" ${
+      enabled ? "checked" : ""
+    }>
+          <span class="checkmark"></span> ${hostnameSafe}
         </label>
-        <small><em>(${timesBlocked}x)</em></small>
-      </li>`)
-  })
-  document.querySelector('ul.websites').innerHTML = websites.join('\n')
+        <small><em>(${parseInt(timesBlocked, 10)}x)</em></small>
+      </li>`);
+  });
+
+  // We only inject HTML which is safe to inject
+  document.querySelector("ul.websites").innerHTML = websitesSafe.join("\n");
 
   blacklist.forEach(({ hostname }) => {
-    document.querySelector(`[id="${hostname}"]`).addEventListener('change', (event) => {
-      const enabled = event.target.checked
-      const item = blacklist
-      chrome.storage.local.get(['blacklist'], ({ blacklist: blacklistLocal2 }) => {
-        console.log('enabled', enabled)
-        console.log('blacklistLocal2', blacklistLocal2)
-        const newBlacklist = [...blacklistLocal2].map(item => (item.hostname === hostname) ? { ...item, enabled } : item)
-        console.log('blacklistLocal2 2', blacklistLocal2)
-        console.log('newBlacklist', newBlacklist)
-        // blacklist = [ ...newBlacklist ]
-        chrome.storage.local.set({ blacklist: [...newBlacklist] })
-      })
-    })
-  })
-})
+    document
+      .querySelector(`[id="${hostname}"]`)
+      .addEventListener("change", (event) => {
+        const enabled = event.target.checked;
+        chrome.storage.local.get(
+          ["blacklist"],
+          ({ blacklist: blacklistLocal2 }) => {
+            console.log("enabled", enabled);
+            console.log("blacklistLocal2", blacklistLocal2);
+            const newBlacklist = [...blacklistLocal2].map((item) =>
+              item.hostname === hostname ? { ...item, enabled } : item
+            );
+            console.log("blacklistLocal2 2", blacklistLocal2);
+            console.log("newBlacklist", newBlacklist);
+            chrome.storage.local.set({ blacklist: [...newBlacklist] });
+          }
+        );
+      });
+  });
+});
 
-chrome.storage.local.get(['scripts'], ({ scripts: scriptsLocal }) => {
-  const scripts2 = []
-  const nonSaScripts = scriptsLocal.filter(name => name.indexOf('.simpleanalytics.') === -1 && name.indexOf('.simpleanalyticscdn.') === -1)
-  nonSaScripts.forEach(name => {
-    scripts2.push(`
+chrome.storage.local.get(["scripts"], ({ scripts: scriptsLocal }) => {
+  const scriptsSafe = [];
+  const nonSaScripts = scriptsLocal.filter(
+    (name) =>
+      name.indexOf(".simpleanalytics.") === -1 &&
+      name.indexOf(".simpleanalyticscdn.") === -1
+  );
+  nonSaScripts.forEach((name) => {
+    const nameSafe = name.replace(/[^a-z0-9./?:-]+/gi, "");
+    scriptsSafe.push(`
       <li>
-        <label class="checkbox" for="${name}">
-          <input type="checkbox" id="${name}" checked>
-          <span class="checkmark"></span> ${name}
+        <label class="checkbox" for="${nameSafe}">
+          <input type="checkbox" id="${nameSafe}" checked>
+          <span class="checkmark"></span> ${nameSafe}
         </label>
-      </li>`)
-  })
+      </li>`);
+  });
 
-  console.log(nonSaScripts)
   if (nonSaScripts.length > 0) {
-    document.querySelector('[data-js="scripts"]').style.display = 'block'
-    document.querySelector('ul.scripts').innerHTML = scripts2.join('\n')
+    document.querySelector('[data-js="scripts"]').style.display = "block";
+
+    // We only inject HTML which is safe to inject
+    document.querySelector("ul.scripts").innerHTML = scriptsSafe.join("\n");
   }
 
   nonSaScripts.forEach((name) => {
-    document.querySelector(`[id="${name}"]`).addEventListener('change', (event) => {
-      const enabled = event.target.checked
-      chrome.storage.local.get(['scripts'], ({ scripts: scriptsLocal2 }) => {
-        const newScriptsLocal = enabled ? [...scriptsLocal2, name] : [...scriptsLocal2].filter(localName => localName !== name)
-        chrome.storage.local.set({ scripts: [...newScriptsLocal] })
-      })
-    })
-  })
-})
+    document
+      .querySelector(`[id="${name}"]`)
+      .addEventListener("change", (event) => {
+        const enabled = event.target.checked;
+        chrome.storage.local.get(["scripts"], ({ scripts: scriptsLocal2 }) => {
+          const newScriptsLocal = enabled
+            ? [...scriptsLocal2, name]
+            : [...scriptsLocal2].filter((localName) => localName !== name);
+          chrome.storage.local.set({ scripts: [...newScriptsLocal] });
+        });
+      });
+  });
+});
